7bc7b91b7fef32e175fa86b768edb5ec3a436aac
Aryan Saini <09aryan.lucky@gmail.com>
2025-09-20 11:48:58 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+  // checking that item is present in cart ot not
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
